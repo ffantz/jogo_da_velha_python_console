71ea99941afc4a269557adca67b7ff7c3e189075
Flavio Caetano <flavioluzio22@gmail.com>
2018-12-09 19:34:09 -0200

    Adição de comentários

--- jogo_da_velha.py
+++ jogo_da_velha.py
@@ -1,10 +1,11 @@
 #Python 	- Jogo da velha
 #Author 	- Flavio
 
-import random	#
-import os
-clear = lambda: os.system('cls')
+import random						#Biblioteca para geração de números aleatório
+import os							#Biblioteca do sistema
+clear = lambda: os.system('cls')	#Cria um lambda para limpar o console
 
+#Função para limpar a tela e exibir o tabuleiro atual
 def display_board(board):
 	clear()
 	print(board[0] + " | " + board[1] + " | " + board[2])
@@ -13,38 +14,42 @@ def display_board(board):
 	print("--+---+--")
 	print(board[6] + " | " + board[7] + " | " + board[8])
 
-#display_board(["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-
+#Função para definir qual player jogará com qual símbolo
 def player_input():
+	#Define um marcador vazio para entrar no loop de verificação
 	marker = ''
+	
+	#Loop de verificação de escolha do marcador
+	#Garante que o jogador enviará uma opção válida
 	while not (marker == '1' or marker == '2'):
 		marker = input("Player 1: jogar como X ou O? (1 ou 2)\n")
 		
 	if marker == '1':
+		#Retorna a tupla com os marcadores para Player 1 e Player 2
 		return ('X', 'O')
 	else:
 		return ('O', 'X')
-		
-def place_marker(board, marker, position, turno):
+	
+#Função para definir uma marcação no tabuleiro	
+def place_marker(board, marker, position):
+	#Verifica se a posição já não está marcada
 	if board[position] != "":
 		board[position] = marker
-		if turno:
-			turno = False
-		else:
-			turno = True
+		
 	else:
 		print("Escolha uma posição válida!")
 		
+#Função para verificar se um dos jogadores ganhou
 def win_check(board, mark):
 	return (
-	(board[0] == mark and board[1] == mark and board [2]) or
-	(board[3] == mark and board[4] == mark and board [5]) or
-	(board[6] == mark and board[7] == mark and board [8]) or
-	(board[0] == mark and board[3] == mark and board [6]) or
-	(board[1] == mark and board[4] == mark and board [7]) or
-	(board[2] == mark and board[5] == mark and board [8]) or
-	(board[0] == mark and board[4] == mark and board [8]) or
-	(board[2] == mark and board[4] == mark and board [6]) or
+	(board[0] == mark and board[1] == mark and board [2]) or	#Vitória na primeira linha
+	(board[3] == mark and board[4] == mark and board [5]) or	#Vitória na segunda linha
+	(board[6] == mark and board[7] == mark and board [8]) or	#Vitória na terceira linha
+	(board[0] == mark and board[3] == mark and board [6]) or	#Vitória na primeira coluna
+	(board[1] == mark and board[4] == mark and board [7]) or	#Vitória na segunda coluna
+	(board[2] == mark and board[5] == mark and board [8]) or	#Vitória na terceira coluna
+	(board[0] == mark and board[4] == mark and board [8]) or	#Vitória na diagonal principal
+	(board[2] == mark and board[4] == mark and board [6]) or	#Vitória na diagonal secundária
 	)		
 	
 
